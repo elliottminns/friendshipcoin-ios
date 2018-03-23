f6ff2c55a1acebafdfd94bb88d328813628023a1
Elliott Minns <elliott.minns@me.com>
2018-03-23 10:08:15 -0500

    Initial Commit

---
+++ FriendshipCoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FriendshipCoin
+//
+//  Created by Elliott Minns on 23/03/2018.
+//  Copyright © 2018 FriendshipCoin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
